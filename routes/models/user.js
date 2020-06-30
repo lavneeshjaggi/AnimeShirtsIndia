c76b2a4d53c0850652a5a51da6b0d5dd90adf07a
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-06-30 14:27:27 +0530

    create user model

---
+++ routes/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+
+    },
+    cart: [
+        {
+            type: String
+        }
+    ],
+    createdOn: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = User = UserSchema;
