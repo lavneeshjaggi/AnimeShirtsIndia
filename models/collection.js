d0ee5ff93f437af117639cb1107fe892e3905403
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-07-09 00:56:02 +0530

    Update collection model and delete unnecessary react stuff

--- models/collection.js
+++ models/collection.js
@@ -1,54 +1,50 @@
 const mongoose = require('mongoose');
 
 const CollectionSchema = new mongoose.Schema({
-    category: [
+    title: {
+        type: String
+    },
+    items: [
         {
             title: {
                 type: String
             },
-            items: [
+            imageUrl: {
+                type: String
+            },
+            description: {
+                type: String
+            },
+            sizes: [
                 {
-                    title: {
+                    size: {
                         type: String
                     },
-                    imageUrl: {
+                    quantity: {
                         type: String
                     },
-                    description: {
+                    price: {
+                        type: Number
+                    }
+                }
+            ],
+            reviews: [
+                {
+                    user: {
+                        type: mongoose.Schema.Types.ObjectId,
+                        ref: 'Users'
+                    },
+                    content: {
                         type: String
                     },
-                    sizes: [
-                        {
-                            size: {
-                                type: String
-                            },
-                            quantity: {
-                                type: String
-                            },
-                            price: {
-                                type: Number
-                            }
-                        }
-                    ],
-                    reviews: [
+                    likes: [
                         {
                             user: {
                                 type: mongoose.Schema.Types.ObjectId,
                                 ref: 'Users'
-                            },
-                            content: {
-                                type: String
-                            },
-                            likes: [
-                                {
-                                    user: {
-                                        type: mongoose.Schema.Types.ObjectId,
-                                        ref: 'Users'
-                                    }
-                                }
-                            ],
+                            }
                         }
-                    ]
+                    ],
                 }
             ]
         }
