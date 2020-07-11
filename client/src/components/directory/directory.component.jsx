055a8dab57f9b180945bfff1df28d314e3b8db04
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-07-12 00:30:07 +0530

    Update categories

--- client/src/components/directory/directory.component.jsx
+++ client/src/components/directory/directory.component.jsx
@@ -12,32 +12,32 @@ class Directory extends React.Component {
             sections: [
                 {
                     id: 1,
-                    title: 't-shirts',
-                    imageUrl: 'https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80'
-                },
-                {
-                    id: 2,
                     title: 'jackets',
                     imageUrl: 'https://images.unsplash.com/photo-1489286696299-aa7486820bd5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80'
                 },
                 {
-                    id: 3,
+                    id: 2,
                     title: 'art work',
                     imageUrl: 'https://images.unsplash.com/photo-1582555172866-f73bb12a2ab3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80'
                 },
                 {
+                    id: 3,
+                    title: 'accessories',
+                    imageUrl: 'https://images.unsplash.com/3/www.madebyvadim.com.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+                },
+                {
                     id: 4,
-                    title: 'mens',
-                    imageUrl: 'https://images.unsplash.com/photo-1484515991647-c5760fcecfc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=387&q=80',
+                    title: 't-shirts',
+                    imageUrl: 'https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
                     size: 'large'
                 },
                 {
                     id: 5,
-                    title: 'womens',
-                    imageUrl: 'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80',
+                    title: 'hoodies',
+                    imageUrl: 'https://images.unsplash.com/photo-1512919768487-e3c8ea8d4567?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
                     size: 'large'
                 }
-            ]
+            ],
         }
     }
 
@@ -46,7 +46,12 @@ class Directory extends React.Component {
             <div className='directory'>
                 {
                     this.state.sections.map(({ id, title, imageUrl, size }) => (
-                        <Categories key={id} title={title} imageUrl={imageUrl} size={size} />
+                        <Categories 
+                            key={id} 
+                            title={title} 
+                            imageUrl={imageUrl} 
+                            size={size} 
+                        />
                     ))
                 }
             </div>   
