338c1bbc1f3eb43fc5ed8a87a3a9ec1e0e796ab9
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-07-11 13:35:23 +0530

    Add routing and change fonts

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 import Homepage from './pages/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+    <h1>T-SHIRTS</h1>
+  </div>
+)
+
 const App = () => {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
