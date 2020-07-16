1d44ca05192bd8dd2df5cc1fd90b37e3a1ee53e4
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-07-16 15:00:38 +0530

    Change carousel urls and add styles to carousel

--- client/src/components/carousel/carousel.component.jsx
+++ client/src/components/carousel/carousel.component.jsx
@@ -5,27 +5,33 @@ import './carousel.styles.scss';
 var images = [
     {
         id: 1,
-        imageUrl: 'https://images.unsplash.com/photo-1512436991641-6745cdb1723f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        imageUrl: 'https://images.unsplash.com/photo-1477659803863-c1bf91b34c90?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        msg: 'Halloween Sale'
     },
     {
         id: 2,
-        imageUrl: 'https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        imageUrl: 'https://images.unsplash.com/photo-1577083753695-e010191bacb5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        msg: 'Diwali Sale'
     },
     {
         id: 3,
-        imageUrl: 'https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        imageUrl: 'https://images.unsplash.com/photo-1545048702-79362596cdc9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        msg: 'Christamas Sale'
     },
     {
         id: 4,
-        imageUrl: 'https://images.unsplash.com/photo-1490481651871-ab68de25d43d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        imageUrl: 'https://images.unsplash.com/photo-1587642303371-96accbf47448?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        msg: 'Holi Sale'
     },
     {
         id: 5,
-        imageUrl: 'https://images.unsplash.com/photo-1503342217505-b0a15ec3261c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        imageUrl: 'https://images.unsplash.com/photo-1590337797479-f208d2ae1965?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        msg: 'Eid Sale'
     },
     {
         id: 6,
-        imageUrl: 'https://images.unsplash.com/photo-1537832816519-689ad163238b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        imageUrl: 'https://images.unsplash.com/photo-1535982330050-f1c2fb79ff78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+        msg: 'Back to School Sale'
     }
 ];
 
@@ -40,28 +46,38 @@ const Carousel = () => {
 
     return (
         <div className='carousel'>
-                {images.map(({ id, imageUrl }) => (
+            {images.map(({ id, imageUrl, msg }) => (
+                <div 
+                    key={id} 
+                    className='slider'
+                    style={{
+                        transform: `translateX(${x}%)`
+                    }}
+                >
                     <div 
-                        key={id}
                         className='img' 
                         style={{
-                            backgroundImage: `url(${imageUrl})`,
-                            transform: `translateX(${x}%)`,
+                            backgroundImage: `url(${imageUrl})`
                         }} 
-                    />
-                ))}
-                <button 
-                    id='left' 
-                    onClick={goLeft}
-                >
-                    &lt;
-                </button>
-                <button 
-                    id='right' 
-                    onClick={goRight}
-                >
-                    &gt;
-                </button>
+                    />,
+                    <div className='msg'>
+                        <h1 className='title'>{msg.toUpperCase()}</h1>
+                        <h2 className='subtitle'>SHOP NOW</h2>
+                    </div>
+                </div>
+            ))}
+            <button 
+                id='left' 
+                onClick={goLeft}
+            >
+                &lt;
+            </button>
+            <button 
+                id='right' 
+                onClick={goRight}
+            >
+                &gt;
+            </button>
         </div>
     )
 };
