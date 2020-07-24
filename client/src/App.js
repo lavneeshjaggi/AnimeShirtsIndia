20879471ee110602c627a73c7415412d715c9b70
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-07-25 01:30:02 +0530

    Add basic sign in page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import Header from './components/header/header.component';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
       <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/shop' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} /> 
+        <Route path='/signin' component={SignInAndSignUp} /> 
       </Switch>
     </div>
   );
