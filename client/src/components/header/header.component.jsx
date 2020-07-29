eb136e8c98abffef0bb7cf9cc35626ebd90df7a5
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-07-29 14:49:20 +0530

    Update header component code

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 import  { ReactComponent as Logo } from '../../assets/yellow-raven.svg';
@@ -7,24 +7,6 @@ import  { ReactComponent as Logo } from '../../assets/yellow-raven.svg';
 import './header.styles.scss';
 
 class Header extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            redirectTo: null
-        }
-    }
-
-    redirect = async () => {
-        const response = await axios.get('/authenticated');
-
-        if (response.data.user) {
-            this.setState({
-                redirectTo: '/'
-            });
-        }
-    }
-
     getUser = async () => {
         return await axios.get('/authenticated');
     }
@@ -52,49 +34,43 @@ class Header extends React.Component {
             const body = JSON.stringify(data);
 
             await axios.post('/logout', body, config);
-
-            this.redirect();
         } catch (error) {
             alert(error.response.data);
         }
     }
 
     render() {
-        if (this.state.redirectTo) {
-            return <Redirect to={this.state.redirectTo} />
-        } else {
-            return (    
-                <div className='header'>
-                    <Link className='logo-container' to='/'>
-                        <Logo className='logo' />
+        return (    
+            <div className='header'>
+                <Link className='logo-container' to='/'>
+                    <Logo className='logo' />
+                </Link>
+                <div className='options'>
+                    <Link className='option' to='/shop'>
+                        Shop
                     </Link>
-                    <div className='options'>
-                        <Link className='option' to='/shop'>
-                            Shop
-                        </Link>
-                        <Link className='option' to='/contact'>
-                            Contact
-                        </Link>
-                        {
-                            this.props.loggedIn 
-                            ?
-                            <Link className='option' to='#' onClick={this.logOut}>Sign Out</Link> 
-                            :
-                            (
-                                <div>
-                                    <Link className='option' to='/signin'>
-                                        Sign In
-                                    </Link>
-                                    <Link to='signup'>
-                                        Sign Up
-                                    </Link>
-                                </div>
-                            )
-                        }
-                    </div>
+                    <Link className='option' to='/contact'>
+                        Contact
+                    </Link>
+                    {
+                        this.props.loggedIn 
+                        ?
+                        <Link className='option' to='#' onClick={this.logOut}>Sign Out</Link> 
+                        :
+                        (
+                            <div>
+                                <Link className='option' to='/signin'>
+                                    Sign In
+                                </Link>
+                                <Link to='signup'>
+                                    Sign Up
+                                </Link>
+                            </div>
+                        )
+                    }
                 </div>
-            );
-        }
+            </div>
+        );
     }
 };
 
