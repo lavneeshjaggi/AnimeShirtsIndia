79097a530d3372788730734e616b6ba303161e24
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-07-30 19:07:31 +0530

    Disconnect app component from redux and connect homepage component to redux which fixes an authentication bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import axios from 'axios';
-import { connect } from 'react-redux';
 
 import { Switch, Route } from 'react-router-dom';
 
@@ -12,55 +10,19 @@ import ContactPage from './pages/contact/contact.component';
 import SignInPage from './pages/sign-in/sign-in.component'; 
 import SignUpPage from './pages/sign-up/sign-up.component'; 
 
-import { setCurrentUser } from './redux/user/user.actions';
-
 import './App.css';
 
-class App extends React.Component {
-  componentDidMount() {
-    this.isLoggedIn();
-  }
+const App  = () => (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} /> 
+        <Route path='/contact' component={ContactPage} /> 
+        <Route path='/signin' component={SignInPage} /> 
+        <Route path='/signup' component={SignUpPage} /> 
+      </Switch>
+    </div>
+)
 
-  isLoggedIn = async () => {
-    const { setCurrentUser } = this.props;
-
-    const response = await axios.get('/authenticated');
-
-    const user = response.data.user;
-
-    if (user) {
-      setCurrentUser({
-        id: user._id,
-        cart: user.cart,
-        name: user.name,
-        email: user.email
-      });
-    }
-
-    setCurrentUser(user);
-  }
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={Homepage} />
-          <Route path='/shop' component={ShopPage} /> 
-          <Route path='/contact' component={ContactPage} /> 
-          <Route path='/signin' component={SignInPage} /> 
-          <Route path='/signup' component={SignUpPage} /> 
-        </Switch>
-      </div>
-    );
-  }
-}
-
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user)) 
-});
-
-export default connect(
-  null, 
-  mapDispatchToProps
-)(App);
+export default App;
