4d0d49eb39709fe01e67ccccc9be0ab15e4bd0fe
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-08-04 18:38:45 +0530

    Add user selectors to avoid rerenders

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { createStructuredSelector } from 'reselect';
 
 import { ReactComponent as Logo } from '../../assets/yellow-raven.svg';
 
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import './header.styles.scss';
 
@@ -77,9 +81,9 @@ const Header  = ({ currentUser, hidden }) => {
     );   
 };
 
-const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 }); 
 
 export default connect(mapStateToProps)(Header);
