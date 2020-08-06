ec8baff38ec44e238193169faff4ec314aa1eb87
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-08-06 21:12:41 +0530

    Add persistence to the app and remove some unnecassary files

--- client/src/redux/root-reducer.js
+++ client/src/redux/root-reducer.js
@@ -1,11 +1,19 @@
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
 
 const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [thunk];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
