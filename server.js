08ee44784cddc3ddafd9db5f09d3099df1af5ccc
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-09-01 01:14:01 +0530

    Update server.js code for production

--- server.js
+++ server.js
@@ -35,10 +35,10 @@ app.use("/logout", require("./routes/sign-out"));
 app.use("/register", require("./routes/sign-up"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, 'client/build'));
 
   app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
