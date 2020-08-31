91658e8283ae598876af85510206ad64d9737d04
lavneeshjaggi <lavneesh.jaggi25@gmail.com>
2020-08-31 18:51:47 +0530

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express"),
+  path = require("path"),
   compression = require("compression"),
   session = require("express-session"),
   config = require("config"),
@@ -32,6 +33,14 @@ app.use("/authenticated", require("./routes/authenticated"));
 app.use("/login", require("./routes/sign-in"));
 app.use("/logout", require("./routes/sign-out"));
 app.use("/register", require("./routes/sign-up"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
