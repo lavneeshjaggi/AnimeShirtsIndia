1255e34171dc9cd2d3be1d833ef0a55ad14375c4
lavneesh-a1office <lavneesh@a1apps.co>
2025-06-03 02:25:06 +0530

    update vite file, upgrade react, react-dom and react-router-dom

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,18 +2,9 @@ import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
   server: {
-    proxy: {
-      // Proxying API requests from /api to http://localhost:5000/api
-      // Adjust the '/api' path if your client makes requests to a different base path
-      "/api": {
-        // Or any other path your client uses to call the backend
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
+    proxy: { "/api": { changeOrigin: true, target: "http://localhost:5000" } },
   },
 });
